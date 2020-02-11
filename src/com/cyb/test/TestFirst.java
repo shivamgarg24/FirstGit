fb484ea2f16aabf1f1dfde4cd2153a55824e5a9e
shivamgarg24 <55242011+shivamgarg24@users.noreply.github.com>
2020-02-11 23:59:54 +0530

    Update TestFirst.java

--- src/com/cyb/test/TestFirst.java
+++ src/com/cyb/test/TestFirst.java
@@ -2,6 +2,6 @@ package com.cyb.test;
 
 public class TestFirst {
 public static void main(String[] args) {
-	System.out.println("Firts Commit");
+	System.out.println("Firts Commit in master");
 }
 }
