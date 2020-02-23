35ab0b5d3bbb6223b1bb53a7d6895c7ca2fb94b3
unknown <sg24shivam.garg@gmail.com>
2020-02-23 15:40:59 +0530

    Commiting first class for branch

--- src/com/cyb/test/TestFirst.java
+++ src/com/cyb/test/TestFirst.java
@@ -1,7 +1,38 @@
 package com.cyb.test;
 
+import java.util.Scanner;
+
 public class TestFirst {
 public static void main(String[] args) {
-	System.out.println("Changed to observe fetch");
+    
+   Scanner sc=new Scanner(System.in);
+    String a=sc.next();
+    String b=sc.next();
+    /* Enter your code here. Print output to STDOUT. */
+    
+    
+    System.out.println(a.length()+b.length());
+    int x=a.compareTo(b);
+    if(x<0 || x==0){
+        System.out.println("No");
+}
+    else{
+    	 System.out.println("Yes");
+    }
+   StringBuffer auffer = new StringBuffer();
+StringBuffer buffer = new StringBuffer();
+int len = 1; 
+while(len<=a.length()-1){
+auffer.append(a.charAt(len++));
+}
+int lenb = 1; 
+while(lenb<=b.length()-1){
+buffer.append(b.charAt(lenb++));
+}
+System.out.print(a.toUpperCase().charAt(0)+""+auffer+" "+b.toUpperCase().charAt(0)+""+buffer);  
+     
+   
+
 }
 }
+
---
+++ src/com/cyb/test/TestString2.java
@@ -0,0 +1,26 @@
+package com.cyb.test;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class TestString2 {
+
+    public static void main(String[] args) {
+    	String str = "Paris in the the spring";
+    	Pattern pattern;
+        Matcher matcher;
+     
+        final String FND_PATTERN = 
+    		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+    		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+    		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+    		"([01]?\\d\\d?|2[0-4]\\d|25[0-5])";
+    	  
+    	
+    }
+}
